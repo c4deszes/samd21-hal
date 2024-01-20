f8a9af02caff895de0be56cc153bbb09b569b117
Balazs Eszes <c4deszes@gmail.com>
2024-01-20 20:54:34 +0100

    NVMCTRL component

---
+++ include/hal/nvmctrl.h
@@ -0,0 +1,21 @@
+#if !defined(HAL_NVMCTRL_H_)
+#define HAL_NVMCTRL_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void NVMCTRL_EraseRow(uint16_t row);
+
+void NVMCTRL_PageBufferClear(void);
+
+void NVMCTRL_WritePage(void);
+
+void NVMCTRL_SetAutoPageWrite(bool enabled);
+
+void NVMCTRL_SetReadWaitStates(uint8_t wait_states);
+
+uint16_t NVMCTRL_GetPageSize(void);
+
+uint16_t NVMCTRL_GetPageCount(void);
+
+#endif // HAL_NVMCTRL_H_
---
+++ src/hal/nvmctrl.c
@@ -0,0 +1,42 @@
+#include "hal/nvmctrl.h"
+
+#include "atsamd21e18a.h"
+
+void NVMCTRL_EraseRow(uint16_t row) {
+    NVMCTRL_REGS->NVMCTRL_ADDR = (row * 128UL * 4UL) / 2;
+    NVMCTRL_REGS->NVMCTRL_CTRLA = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_ER;
+
+    while ((NVMCTRL_REGS->NVMCTRL_INTFLAG & NVMCTRL_INTFLAG_READY_Msk) == 0);
+}
+
+void NVMCTRL_PageBufferClear(void) {
+    NVMCTRL_REGS->NVMCTRL_CTRLA = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_PBC;
+    while ((NVMCTRL_REGS->NVMCTRL_INTFLAG & NVMCTRL_INTFLAG_READY_Msk) == 0);
+}
+
+void NVMCTRL_WritePage(void) {
+    NVMCTRL_REGS->NVMCTRL_CTRLA = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_WP;
+    while ((NVMCTRL_REGS->NVMCTRL_INTFLAG & NVMCTRL_INTFLAG_READY_Msk) == 0);
+}
+
+void NVMCTRL_SetAutoPageWrite(bool enabled) {
+    if (enabled) {
+        NVMCTRL_REGS->NVMCTRL_CTRLB &= ~(NVMCTRL_CTRLB_MANW_Msk);
+    }
+    else {
+        NVMCTRL_REGS->NVMCTRL_CTRLB |= NVMCTRL_CTRLB_MANW_Msk;
+    }
+}
+
+void NVMCTRL_SetReadWaitStates(uint8_t wait_states) {
+    // TODO: consider the input value
+    NVMCTRL_REGS->NVMCTRL_CTRLB |= NVMCTRL_CTRLB_RWS_HALF_Val;
+}
+
+uint16_t NVMCTRL_GetPageSize(void) {
+    return (1 << (3 + (NVMCTRL_REGS->NVMCTRL_PARAM & NVMCTRL_PARAM_PSZ_Msk >> NVMCTRL_PARAM_PSZ_Pos)));
+}
+
+uint16_t NVMCTRL_GetPageCount(void) {
+    return NVMCTRL_REGS->NVMCTRL_PARAM & NVMCTRL_PARAM_NVMP_Msk >> NVMCTRL_PARAM_NVMP_Pos;
+}

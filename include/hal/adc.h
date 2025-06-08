cf7354acc052f1fc7dd70e0317bdbbc7624c252b
Balazs Eszes <c4deszes@gmail.com>
2025-06-08 21:33:30 +0200

    NVMCTRL page write ADC reading

--- include/hal/adc.h
+++ include/hal/adc.h
@@ -3,11 +3,18 @@
 
 #include <stdint.h>
 
-// Loads calib values from NVRAM
-void ADC_LoadCalibration(void);
+typedef void (*adc_result_callback_t)(uint16_t result);
 
-void ADC_SetupFreerun(uint8_t channel);
+//void ADC_SetupFreerun(void);
 
-uint16_t ADC_ImmediateRead(uint8_t channel);
+void ADC_SetupSingleShot(void);
+
+void ADC_Stop(void);
+
+uint16_t ADC_ReadSync(uint8_t channel);
+
+void ADC_ReadAsync(uint8_t channel, adc_result_callback_t callback);
+
+void ADC_Reset(void);
 
 #endif // HAL_ADC_H_
